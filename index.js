e08f650d60aa6ce480f278cc4c2ce2b79829d97e
icnpdesign <inp@bu.edu>
2017-11-08 23:43:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Write your solution in this file!
 const driver = {
   name : 'Ben',
-  age: '23'
+  
 }
 
 function updateDriverWithKeyAndValue(driver, newDriver) {
-  delete newDriver.name;
+  const newDriver = {
+    
+  }
 }
