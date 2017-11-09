6e37ad84512b54d283d81e0ee7d9bca1442fb5d4
icnpdesign <inp@bu.edu>
2017-11-09 00:02:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // Write your solution in this file!
-const driver = {
-  name : 'Sam',
-}
 
 function updateDriverWithKeyAndValue() {
   return Object.assign({}, driver, { name: 'Sam'}{ address: '11 Broadway'});
