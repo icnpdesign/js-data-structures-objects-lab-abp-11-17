302201ce8c4ff9dce7dd771d951573c84534296a
icnpdesign <inp@bu.edu>
2017-11-09 00:43:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,5 @@ const driver = {
   name : 'Bill',
 }
 
-const newDriver = {
-  name : 'Sam',
-  address : '11 Broadway'
-};
 
 const updatedDriver = updateDriverWithKeyAndValue(driver, 'address', '11 Broadway');
