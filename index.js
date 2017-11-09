14b8a3dcac17ec8f44f1a2e8fb09a980e828e81c
icnpdesign <inp@bu.edu>
2017-11-09 00:24:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 const driver = {
   name : 'Sam',
-};
+}
 
 function updateDriverWithKeyAndValue(driver, newDriver) {
   return Object.assign ( {}, driver, newDriver);
