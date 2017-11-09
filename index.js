eda6438bdfaee939af341140d76a7318b27bfdf7
icnpdesign <inp@bu.edu>
2017-11-09 01:15:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function deleteFromDriverByKey() {
   return updatedDriver;
 }
 
-function destructivelyDeleteFromdriverByKey (driver, key) {
+function destructivelyDeleteFromDriverByKey (driver, key) {
   delete driver[key];
   return driver;
 }
