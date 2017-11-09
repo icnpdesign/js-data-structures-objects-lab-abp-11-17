a7e5a9f9ff2ac40aee81f877ed72dabc3b5063e3
icnpdesign <inp@bu.edu>
2017-11-09 01:03:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyUpdateDriverWithKeyAndValue() {
 }
 
 function deleteFromDriverByKey() {
-  const newDriver = Object.assign ({}, driver, delete driver.name)
+  const newDriver = Object.assign ({}, driver {delete driver.name})
   return newDriver;
 }
